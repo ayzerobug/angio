30256e2e64213e3e0fe9a62a90d3d39425ebbe09
Aydcoder <aydcoder@gmail.com>
2022-09-09 15:34:25 +0100

    User model

---
+++ lib/utils/Models/User.dart
@@ -0,0 +1,13 @@
+class User {
+  final String username;
+  final String name;
+  final bool isVerified;
+  final String avatarUrl;
+
+  User({
+    required this.username,
+    required this.name,
+    required this.isVerified,
+    required this.avatarUrl,
+  });
+}
